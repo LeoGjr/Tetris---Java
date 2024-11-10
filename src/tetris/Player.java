9d61c3cf1d6dfd4cbf88b23a38b2ff367fe1e831
LeoGjr <111541869+LeoGjr@users.noreply.github.com>
2024-11-10 11:12:04 -0800

    first commit

---
+++ src/tetris/Player.java
@@ -0,0 +1,66 @@
+package tetris;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Player {
+
+	public boolean right,left;
+	
+	public int x,y;
+	
+	public Bloco bloco;
+	
+	public boolean changeShape = false;
+	
+	public Player(int x,int y) {
+		this.x = x;
+		this.y = y;
+		
+		bloco = new Bloco(x,y);
+	}
+	
+	public boolean canGo(int nextx,int nexty) {
+		if(x < 0) {
+			x = 0;
+			return false;
+		}
+		
+		
+		int ww = Shapes.shapes.get(bloco.shape).width;
+		if(x + ww > 480) {
+			x = 480 - ww;
+			return false;
+		}
+		
+
+		//int hh = Shapes.shapes.get(bloco.shape).height;
+		return true;
+	}
+	
+
+	public void tick() {
+		if(right && canGo(x+4,y)) {
+			x+=4;
+		}else if(left && canGo(x-4,y)) {
+			x-=4;
+		}
+		
+		if(changeShape) {
+			changeShape = false;
+			bloco.shape++;
+			if(bloco.shape == Shapes.shapes.size()) {
+				bloco.shape = 0;
+			}
+		}
+		
+		bloco.x = x;
+		bloco.tick();
+		
+	}
+	
+	public void render(Graphics g) {
+		bloco.render(g);
+	}
+	
+}
---
+++ src/tetris/Shapes.java
@@ -0,0 +1,18 @@
+package tetris;
+
+import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Shapes {
+
+	public static Random random = new Random();
+	public static ArrayList<Rectangle> shapes = new ArrayList<Rectangle>();
+	
+	public Shapes() {
+		shapes.add(new Rectangle(0,0,300,20));
+		shapes.add(new Rectangle(0,0,20,100));
+		shapes.add(new Rectangle(0,0,50,50));
+		shapes.add(new Rectangle(0,0,50,50));
+	}
+}
